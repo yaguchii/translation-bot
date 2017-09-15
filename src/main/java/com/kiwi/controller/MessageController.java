1a78332b83036197836a85f344b409aa14abb494
yaguchi <tyaguchi@p.terrasky.com>
2017-09-15 09:35:10 +0900

    commit

--- src/main/java/com/kiwi/controller/MessageController.java
+++ src/main/java/com/kiwi/controller/MessageController.java
@@ -1,14 +1,18 @@
 package com.kiwi.controller;
 
 import com.linecorp.bot.client.LineMessagingServiceBuilder;
+import com.linecorp.bot.model.PushMessage;
 import com.linecorp.bot.model.ReplyMessage;
 import com.linecorp.bot.model.event.*;
 import com.linecorp.bot.model.event.message.TextMessageContent;
+import com.linecorp.bot.model.message.AudioMessage;
 import com.linecorp.bot.model.message.TextMessage;
 import com.linecorp.bot.model.response.BotApiResponse;
 import com.linecorp.bot.spring.boot.annotation.EventMapping;
 import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
 import io.github.firemaples.language.Language;
+import io.github.firemaples.language.SpokenDialect;
+import io.github.firemaples.speak.Speak;
 import io.github.firemaples.translate.Translate;
 import lombok.extern.slf4j.Slf4j;
 import retrofit2.Response;
@@ -60,14 +64,16 @@ public class MessageController {
 
         Translate.setSubscriptionKey(System.getenv("MS_SUBSCRIPTION_KEY"));
         String translatedText = Translate.execute(event.getMessage().getText(), Language.JAPANESE, Language.SPANISH);
-
         replyMessage(event.getReplyToken(), translatedText);
+
+        String speak = Speak.execute(translatedText, SpokenDialect.SPANISH_SPAIN);
+        sendAudio(event.getSource().getSenderId(), speak);
     }
 
     private void replyMessage(String replyToken, String message) throws Exception {
 
         TextMessage textMessage = new TextMessage(message);
-        ReplyMessage  replyMessage = new ReplyMessage (
+        ReplyMessage replyMessage = new ReplyMessage(
                 replyToken,
                 textMessage
         );
@@ -80,4 +86,19 @@ public class MessageController {
         log.info(response.code() + " " + response.message());
     }
 
+    private void sendAudio(String destination, String url) throws Exception {
+
+        AudioMessage audioMessage = new AudioMessage(url, 10_000);
+        PushMessage pushMessage = new PushMessage(
+                destination,
+                audioMessage
+        );
+        Response<BotApiResponse> response =
+                LineMessagingServiceBuilder
+                        .create(System.getenv("LINE_BOT_CHANNEL_TOKEN"))
+                        .build()
+                        .pushMessage(pushMessage)
+                        .execute();
+        log.info(response.code() + " " + response.message());
+    }
 }
